30197cb4b6bc1b49d942579c3b59dfb85602c390
Daniel J. Antonio <djedidiaha@gmail.com>
2022-08-29 20:50:56 +0700

    add gradient on top of navbar

--- src/components/common/header.common.tsx
+++ src/components/common/header.common.tsx
@@ -6,23 +6,23 @@ import { useRouter } from 'next/router';
 const Header: FC = () => {
 	const { pathname } = useRouter();
 	return (
-		<nav className={styles['nav']}>
-			<Link href='/'>Jedidiah.dev</Link>
-			<div className={styles['nav-items']}>
-				<Link href='/about'>
-					<a className={pathname === '/about' ? styles['nav-active'] : ''}>About</a>
-				</Link>
-				<Link href='/projects'>
-					<a className={pathname === '/projects' ? styles['nav-active'] : ''}>Projects</a>
-				</Link>
-				<Link href='/tools'>
-					<a className={pathname === '/tools' ? styles['nav-active'] : ''}>Tools</a>
-				</Link>
-				<Link href='/colors'>
-					<a className={pathname === '/colors' ? styles['nav-active'] : ''}>Colors</a>
-				</Link>
-			</div>
-		</nav>
+		<div className={styles['nav-container']}>
+			<div className={styles['nav-gradient']}></div>
+			<nav className={styles['nav']}>
+				<Link href='/'>Jedidiah.dev</Link>
+				<div className={styles['nav-items']}>
+					<Link href='/about'>
+						<a className={pathname === '/about' ? styles['nav-active'] : ''}>About</a>
+					</Link>
+					<Link href='/projects'>
+						<a className={pathname === '/projects' ? styles['nav-active'] : ''}>Projects</a>
+					</Link>
+					<Link href='/tools'>
+						<a className={pathname === '/tools' ? styles['nav-active'] : ''}>Tools</a>
+					</Link>
+				</div>
+			</nav>
+		</div>
 	);
 };
 
