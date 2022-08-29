76a22423dfff1bd10aa25410a410ea72b07e54e3
Daniel J. Antonio <djedidiaha@gmail.com>
2022-08-29 13:10:27 +0700

    set up navbar + active detection

--- src/components/common/header.common.tsx
+++ src/components/common/header.common.tsx
@@ -1,15 +1,27 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import styles from '../../styles/components/header.module.scss';
+import { useRouter } from 'next/router';
 
 const Header: FC = () => {
+	const { pathname } = useRouter();
 	return (
-		<nav>
-			<Link href='/'>Jedidiah</Link>
-			<Link href='/about'>About</Link>
-			<Link href='/projects'>Projects</Link>
-			<Link href='/tools'>Tools</Link>
-			<Link href='/colors'>Colors</Link>
-			{/* <div>Toggle Dark Mode</div> */}
+		<nav className={styles['nav']}>
+			<Link href='/'>Jedidiah.dev</Link>
+			<div className={styles['nav-items']}>
+				<Link href='/about'>
+					<a className={pathname === '/about' ? styles['nav-active'] : ''}>About</a>
+				</Link>
+				<Link href='/projects'>
+					<a className={pathname === '/projects' ? styles['nav-active'] : ''}>Projects</a>
+				</Link>
+				<Link href='/tools'>
+					<a className={pathname === '/tools' ? styles['nav-active'] : ''}>Tools</a>
+				</Link>
+				<Link href='/colors'>
+					<a className={pathname === '/colors' ? styles['nav-active'] : ''}>Colors</a>
+				</Link>
+			</div>
 		</nav>
 	);
 };
