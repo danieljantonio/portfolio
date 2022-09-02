cf5745d8c8626a18a39ac5a110067807991945e3
Daniel J. Antonio <djedidiaha@gmail.com>
2022-09-02 20:58:18 +0700

    change copyright date to be dynamically set

--- src/components/common/footer.common.tsx
+++ src/components/common/footer.common.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import s from '../../styles/components/footer.module.scss';
 
 const Footer: FC = () => {
-	return <footer className={s['footer']}>Copyright © 2022 Daniel J. Antonio, Inc. All rights reserved.</footer>;
+	return <footer className={s['footer']}>Copyright © {new Date().getFullYear()} Daniel J. Antonio, Inc. All rights reserved.</footer>;
 };
 
 export default Footer;
