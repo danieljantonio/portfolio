a20e4cd8c99b7cc570df0597fcfb962bd431ac56
Daniel J. Antonio <djedidiaha@gmail.com>
2022-09-02 20:31:42 +0700

    change footer text and size - add xs size to scss variables

--- src/components/common/footer.common.tsx
+++ src/components/common/footer.common.tsx
@@ -2,15 +2,7 @@ import { FC } from 'react';
 import s from '../../styles/components/footer.module.scss';
 
 const Footer: FC = () => {
-	return (
-		<footer className={s['footer']}>
-			This page was created by taking inspiration from{' '}
-			<a href='https://daleanthony.com/' className={s['reference-link']}>
-				Dale Anthony
-			</a>
-			.
-		</footer>
-	);
+	return <footer className={s['footer']}>Copyright © 2022 Daniel J. Antonio, Inc. All rights reserved.</footer>;
 };
 
 export default Footer;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,29 @@
 import { NextPage } from 'next';
+import Card from '../components/common/card.common';
 
 const Projects: NextPage = () => {
-	return <div>Projects</div>;
+	return (
+		<div>
+			<h1 className='title'>Projects</h1>
+			<div></div>
+			<Card
+				title='Shalom Plaques'
+				stack='React, React Native, Express, MongoDB, Stripe'
+				body="An eCommerce app that allows parents to purchase their children's needs from the comfort of their home."
+				href={'https://github.com/jedidiahaaaaa/shalom-plaques'}
+			/>
+			<Card
+				title='Pustaka Jasa'
+				stack='React, React Native, Express, MongoDB, Stripe'
+				body="An eCommerce app that allows parents to purchase their children's needs from the comfort of their home."
+				href={'https://play.google.com/store/apps/details?id=com.pustakajasa.esmartapps&hl=en_SG&gl=US'}
+			/>
+			<Card title='Pustaka Jasa' stack='React, React Native, Express, MongoDB, Stripe' body="An eCommerce app that allows parents to purchase their children's needs from the comfort of their home." href={'https://google.com'} />
+			<Card title='Pustaka Jasa' stack='React, React Native, Express, MongoDB, Stripe' body="An eCommerce app that allows parents to purchase their children's needs from the comfort of their home." href={'https://google.com'} />
+			<Card title='Pustaka Jasa' stack='React, React Native, Express, MongoDB, Stripe' body="An eCommerce app that allows parents to purchase their children's needs from the comfort of their home." href={'https://google.com'} />
+			<Card title='Pustaka Jasa' stack='React, React Native, Express, MongoDB, Stripe' body="An eCommerce app that allows parents to purchase their children's needs from the comfort of their home." href={'https://google.com'} />
+		</div>
+	);
 };
 
 export default Projects;
