2bfd987f16f5bffc3be98965a8678531c3c14004
Daniel J. Antonio <djedidiaha@gmail.com>
2022-09-03 22:45:30 +0700

    add experience to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -37,6 +37,26 @@ const About: NextPage = () => {
 					.
 				</div>
 			</div>
+			<hr />
+			<div>
+				<div className={s['text-container']}>
+					<div className={s['header']}>Work Experience</div>
+					<div className={s['row-experience']}>
+						<div className={s['work-details']}>
+							<div className={s['highlight']}>Freelance</div>
+							<div className={s['work-role']}>Full Stack | Mobile Developer</div>
+						</div>
+						<div className={s['work-date']}>2019 -</div>
+					</div>
+					<div className={s['row-experience']}>
+						<div className={s['work-details']}>
+							<div className={s['highlight']}>Pixlr</div>
+							<div className={s['work-role']}>Web Application Developer</div>
+						</div>
+						<div className={s['work-date']}>2019 - 2021</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
